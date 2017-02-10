04362c16414641a142c9ba4d3dc8b47c79654056
minodisk <daisuke.mino@gmail.com>
2017-02-10 15:56:50 +0900

    Combine files

--- builder.go
+++ builder.go
@@ -1,8 +1,26 @@
 package sqlabble
 
 import (
+	"github.com/minodisk/sqlabble/internal/chunk"
 	"github.com/minodisk/sqlabble/internal/generator"
 	"github.com/minodisk/sqlabble/internal/grammar"
+)
+
+var (
+	CreateTable = chunk.NewCreateTable
+	Select      = chunk.NewSelect
+	InsertInto  = chunk.NewInsertInto
+	Update      = chunk.NewUpdate
+	Delete      = chunk.NewDelete
+
+	Column = chunk.NewColumn
+	C      = Column
+	Table  = chunk.NewTable
+	T      = Table
+
+	And = chunk.NewAnd
+	Or  = chunk.NewOr
+	Not = chunk.NewNot
 )
 
 func Build(c grammar.Statement) (string, []interface{}) {
