c442074f416b5b78d21937c7de83ce6295868d47
minodisk <daisuke.mino@gmail.com>
2017-02-28 16:31:26 +0900

    Add tests for generator

---
+++ token/generator_test.go
@@ -0,0 +1,48 @@
+package token_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/minodisk/sqlabble/internal/diff"
+	"github.com/minodisk/sqlabble/token"
+)
+
+func TestGenerate(t *testing.T) {
+	for i, c := range []struct {
+		tokens          token.Tokens
+		withBreaking    string
+		withoutBreaking string
+	}{
+		{
+			token.Tokens{},
+			``,
+			``,
+		},
+		{
+			token.Tokens{
+				token.LineStart, token.Word("foo"), token.LineEnd,
+				token.LineStart, token.Indent, token.Word("bar"), token.ParenStart, token.Word("a"), token.Comma, token.Word("b"), token.ParenEnd, token.LineEnd,
+				token.LineStart, token.Indent, token.Word("baz"), token.FuncParenStart, token.Word("c"), token.Comma, token.Word("d"), token.FuncParenEnd, token.LineEnd,
+			},
+			`foo
+  bar (a, b)
+  baz(c, d)
+`,
+			`foo bar (a, b) baz(c, d)`,
+		},
+	} {
+		t.Run(fmt.Sprintf("%d WithBreaking", i), func(t *testing.T) {
+			got := token.Generate(c.tokens, token.StandardIndentedFormat)
+			if got != c.withBreaking {
+				t.Error(diff.SQL(got, c.withBreaking))
+			}
+		})
+		t.Run(fmt.Sprintf("%d WithoutBreaking", i), func(t *testing.T) {
+			got := token.Generate(c.tokens, token.StandardFormat)
+			if got != c.withoutBreaking {
+				t.Error(diff.SQL(got, c.withBreaking))
+			}
+		})
+	}
+}
