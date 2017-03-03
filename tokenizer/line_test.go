dce00e84328b664f8b963a988cb189ddc1dfff79
minodisk <daisuke.mino@gmail.com>
2017-03-03 17:29:48 +0900

    Add test

---
+++ tokenizer/line_test.go
@@ -0,0 +1,35 @@
+package tokenizer_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/minodisk/sqlabble/internal/diff"
+	"github.com/minodisk/sqlabble/token"
+	"github.com/minodisk/sqlabble/tokenizer"
+)
+
+func TestLine(t *testing.T) {
+	t.Parallel()
+	for i, c := range []struct {
+		line  tokenizer.Line
+		depth int
+		want  token.Tokens
+	}{
+		{
+			tokenizer.NewLine(),
+			0,
+			token.NewTokens(),
+		},
+	} {
+		i := i
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			t.Parallel()
+			got := c.line.Tokenize(c.depth)
+			if !reflect.DeepEqual(got, c.want) {
+				t.Error(diff.Values(got, c.want))
+			}
+		})
+	}
+}
