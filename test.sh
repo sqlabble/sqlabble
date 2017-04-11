8066221c08f558487e2f5d2602be665b83f2592c
minodisk <daisuke.mino@gmail.com>
2017-04-11 15:55:04 +0900

    Move from Coveralls to Codecov

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
+
+curl -o upload.sh https://codecov.io/bash
+bash ./upload.sh
