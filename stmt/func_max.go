4f6571056b0e819fdc298014e88692520f4b8734
fujiwara402 <fuj.t601216@gmail.com>
2017-10-11 14:46:33 +0900

    Support MAX function

--- stmt/func_max.go
+++ stmt/func_max.go
@@ -1,1 +1,10 @@
 package stmt
+
+import "github.com/minodisk/sqlabble/keyword"
+
+func NewMax(col ValOrColOrFuncOrSub) Func {
+	return Func{
+		name: keyword.Max,
+		args: Args{col},
+	}
+}
--- stmt/func_max_test.go
+++ stmt/func_max_test.go
@@ -1,1 +1,57 @@
-package stmt
+package stmt_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/minodisk/sqlabble/internal/diff"
+	"github.com/minodisk/sqlabble/stmt"
+)
+
+func TestMax(t *testing.T) {
+	t.Parallel()
+	for i, c := range []struct {
+		stmt      stmt.Statement
+		sql       string
+		sqlIndent string
+		values    []interface{}
+	}{
+		{
+			stmt.NewMax(stmt.NewWildcard()),
+			`MAX(*)`,
+			`> MAX(*)
+`,
+			nil,
+		},
+		{
+			stmt.NewMax(stmt.NewColumn("foo")),
+			`MAX("foo")`,
+			`> MAX("foo")
+`,
+			nil,
+		},
+	} {
+		c := c
+		t.Run(fmt.Sprintf("%d Build", i), func(t *testing.T) {
+			t.Parallel()
+			sql, values := b.Build(c.stmt)
+			if sql != c.sql {
+				t.Error(diff.SQL(sql, c.sql))
+			}
+			if !reflect.DeepEqual(values, c.values) {
+				t.Error(diff.Values(values, c.values))
+			}
+		})
+		t.Run(fmt.Sprintf("%d BuildIndent", i), func(t *testing.T) {
+			t.Parallel()
+			sql, values := bi.Build(c.stmt)
+			if sql != c.sqlIndent {
+				t.Error(diff.SQL(sql, c.sqlIndent))
+			}
+			if !reflect.DeepEqual(values, c.values) {
+				t.Error(diff.Values(values, c.values))
+			}
+		})
+	}
+}
